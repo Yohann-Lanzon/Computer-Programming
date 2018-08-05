0bf911f66b2180965ab7cad4359f2346d3894c0e
Yohann-Lanzon <42104085+Yohann-Lanzon@users.noreply.github.com>
2018-08-05 12:28:59 +0800

    Add files via upload

---
+++ Lanzon[1].cpp
@@ -0,0 +1,12 @@
+// cin with strings
+#include <iostream>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string mystr;
+    cout << "Hi! my name is Yohann Lanzon, what's yours? I really love cats by the way, and maybe you are a cat? kiddin'!";
+    getline (cin, mystr);
+    return 0;
+}
